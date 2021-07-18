66a37d75ece67fa42ee0267c76967603c82ea8e1
Luke Shin <shingun@ideasam.net>
2021-07-18 11:43:41 +0900

    Template와 Must 사용법 등

--- main.go
+++ main.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/LukeShin/nomadcoin/blockchain"
 )
 
 const port string = ":4000"
 
-func home(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(rw, "Hello!")
+type homeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
 }
 
+func home(rw http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/home.html"))
+	// tmpl, err := template.ParseFiles("templates/home.html")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
+	tmpl.Execute(rw, data)
+}
 func main() {
 	http.HandleFunc("/", home)
 	fmt.Printf("Listening on Http://localhost%s\n", port)
