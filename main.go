012d240b5d02f95b24230472d92c9a737fb89b8e
Luke Shin <shingun@ideasam.net>
2021-07-18 11:28:58 +0900

    서버 동작시키기 테스트

--- main.go
+++ main.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/LukeShin/nomadcoin/blockchain"
+	"log"
+	"net/http"
 )
 
+const port string = ":4000"
+
+func home(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(rw, "Hello!")
+}
+
 func main() {
-	chain := blockchain.GetBlockchain()
-	chain.AddBlock("2nd Block")
-	chain.AddBlock("3rd Block")
-	chain.AddBlock("4th Block")
-	for _, block := range chain.AllBlocks() {
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %s\n", block.Hash)
-		fmt.Printf("Prev: %s\n", block.PrevHash)
-	}
+	http.HandleFunc("/", home)
+	fmt.Printf("Listening on Http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
