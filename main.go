c417a0a96a7de1fc295430cd6380c3eb4be07a70
Luke Shin <shingun@ideasam.net>
2021-07-18 10:54:40 +0900

    #4.5 Refactoring part Two Done! ^_^

--- main.go
+++ main.go
@@ -1,46 +1,19 @@
 package main
 
 import (
-	"crypto/sha256"
 	"fmt"
+
+	"github.com/LukeShin/nomadcoin/blockchain"
 )
 
-type block struct {
-	data     string
-	hash     string
-	prevHash string
-}
-
-type blockchain struct {
-	blocks []block
-}
-
-func (b *blockchain) getLashHash() string {
-	if len(b.blocks) > 0 {
-		return b.blocks[len(b.blocks)-1].hash
-	}
-	return ""
-}
-
-func (b *blockchain) addBlock(data string) {
-	newBlock := block{data, "", b.getLashHash()}
-	hash := sha256.Sum256([]byte(newBlock.data + newBlock.prevHash))
-	newBlock.hash = fmt.Sprintf("%x", hash)
-	b.blocks = append(b.blocks, newBlock)
-}
-
-func (b *blockchain) listBlock() {
-	for _, block := range b.blocks {
-		fmt.Printf("Data: %s\n", block.data)
-		fmt.Printf("Hash: %s\n", block.hash)
-		fmt.Printf("Prev: %s\n", block.prevHash)
-	}
-}
-
 func main() {
-	chain := blockchain{}
-	chain.addBlock("1st Block")
-	chain.addBlock("2nd Block")
-	chain.addBlock("3rd Block")
-	chain.listBlock()
+	chain := blockchain.GetBlockchain()
+	chain.AddBlock("2nd Block")
+	chain.AddBlock("3rd Block")
+	chain.AddBlock("4th Block")
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("Prev: %s\n", block.PrevHash)
+	}
 }
