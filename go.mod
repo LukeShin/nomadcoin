32de13478e7011e57d074e598dff8c3736ba5109
Luke Shin <shingun@ideasam.net>
2021-07-18 09:52:55 +0900

    #4.2 Our First Blockchain Done!

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/LukeShin/nomadcoin
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+type block struct {
+	data     string
+	hash     string
+	prevHash string
+}
+
+type blockchain struct {
+	blocks []block
+}
+
+func (b *blockchain) getLashHash() string {
+	if len(b.blocks) > 0 {
+		return b.blocks[len(b.blocks)-1].hash
+	}
+	return ""
+}
+
+func (b *blockchain) addBlock(data string) {
+	newBlock := block{data, "", b.getLashHash()}
+	hash := sha256.Sum256([]byte(newBlock.data + newBlock.prevHash))
+	newBlock.hash = fmt.Sprintf("%x", hash)
+	b.blocks = append(b.blocks, newBlock)
+}
+
+func (b *blockchain) listBlock() {
+	for _, block := range b.blocks {
+		fmt.Printf("Data: %s\n", block.data)
+		fmt.Printf("Hash: %s\n", block.hash)
+		fmt.Printf("Prev: %s\n", block.prevHash)
+	}
+}
+
+func main() {
+	chain := blockchain{}
+	chain.addBlock("1st Block")
+	chain.addBlock("2nd Block")
+	chain.addBlock("3rd Block")
+	chain.listBlock()
+}
