15b82f866ff7fa0e98742e52ca2902ab143e5676
Luke Shin <shingun@ideasam.net>
2021-07-18 17:54:16 +0900

    #5.4 Adding A Block (Add POST까지!)

--- main.go
+++ main.go
@@ -9,7 +9,12 @@ import (
 	"github.com/LukeShin/nomadcoin/blockchain"
 )
 
-const port string = ":4000"
+const (
+	port        string = ":4000"
+	templateDir string = "templates/"
+)
+
+var templates *template.Template
 
 type homeData struct {
 	PageTitle string
@@ -17,16 +22,28 @@ type homeData struct {
 }
 
 func home(rw http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/home.html"))
-	// tmpl, err := template.ParseFiles("templates/home.html")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
 	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
-	tmpl.Execute(rw, data)
+	templates.ExecuteTemplate(rw, "home", data)
 }
+
+func add(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		templates.ExecuteTemplate(rw, "add", nil)
+	case "POST":
+		r.ParseForm()
+		data := r.Form.Get("blockData")
+		blockchain.GetBlockchain().AddBlock(data)
+		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
+	}
+}
+
 func main() {
+	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
+	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
 	http.HandleFunc("/", home)
+	http.HandleFunc("/add", add)
+
 	fmt.Printf("Listening on Http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
