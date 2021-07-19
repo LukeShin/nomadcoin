72a1542b792083997fe364d88f85acc422943999
Luke Shin <shingun@ideasam.net>
2021-07-19 23:09:23 +0900

    Adapter 패턴 활용, Middleware 활용!

--- rest/rest.go
+++ rest/rest.go
@@ -56,14 +56,12 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 			Description: "See a block",
 		},
 	}
-	rw.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(rw).Encode(data)
 }
 
 func blocks(rw http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		rw.Header().Add("Content-Type", "application/json")
 		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
 	case "POST":
 		var addBlockBody AddBlockBody
@@ -78,13 +76,20 @@ func block(rw http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(vars["height"])
 	utils.HandleErr(err)
 	block := blockchain.GetBlockchain().GetBlock(id)
-	rw.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(rw).Encode(block)
+}
+
+func jsonContentTypeMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Add("Content-Type", "application/json")
+		next.ServeHTTP(rw, r)
+	})
 }
 
 func Start(aPort int) {
 	router := mux.NewRouter()
 	port = fmt.Sprintf(":%d", aPort)
+	router.Use(jsonContentTypeMiddleware)
 	router.HandleFunc("/", documentation).Methods("GET")
 	router.HandleFunc("/blocks", blocks).Methods("GET", "POST")
 	router.HandleFunc("/blocks/{height:[0-9]+}", block).Methods("GET")
